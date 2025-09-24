e9ff2c21ffa3cccbe2b3a1316c87da72e3f82185
angewarren12 <angedesirecamara@gmail.com>
2025-09-24 15:09:34 +0000

    lll

--- src/components/OrangeHeader.tsx
+++ src/components/OrangeHeader.tsx
@@ -1,9 +1,15 @@
+import logoOrange from '../assets/logo-orange.png';
+
 const OrangeHeader = () => {
   return (
     <header className="bg-header w-full py-4 px-6">
       <div className="max-w-6xl mx-auto">
-        <div className="bg-orange-brand text-white px-3 py-1 rounded text-sm font-medium inline-block">
-          orange
+        <div className="flex items-center">
+          <img 
+            src={logoOrange} 
+            alt="Orange" 
+            className="h-8 w-auto"
+          />
         </div>
       </div>
     </header>
